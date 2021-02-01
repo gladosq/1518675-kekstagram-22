7ac6087562ee41e023cb6a55d70dbbe97cfa8540
gladosq <gladosq@gmail.com>
2021-02-02 01:13:43 +0400

    Исправление ошибок #2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
 function getRandomNumber (min, max) {
   if ((typeof min) == 'number' && (typeof max) == 'number') {
-    if (min < max && min >= 0 && max >= 0) {
-      let randomValues = Math.random();
-      let randomCalculation = min + randomValues * (max + 1 - min);
-      randomCalculation = Math.floor(randomCalculation);
-      return randomCalculation;
+    if (min == Math.floor(min) && max == Math.floor(max)) {
+      if (min < max && min >= 0 && max >= 0) {
+        let randomCalculation = min + Math.random() * (max + 1 - min);
+        randomCalculation = Math.floor(randomCalculation);
+        return randomCalculation;
+      }
     }
-    return alert('Первое число не должно быть меньше или равняться второму!');
   }
-  return alert('Введите корректные данные');
+  return false;
 }
 
-function getStringLength (string, maxStringSize) {
+function lengthStringCheck (string, maxStringSize) {
   if ((typeof string) == 'string' && (typeof maxStringSize) == 'number') {
-    if (string.length > maxStringSize) {
-      return false;
-    }
-    return true;
+    return (string.length < maxStringSize);
   }
-  return alert('Введите корректные данные');
+  return false;
 }
 
-getRandomNumber();
-getStringLength();
+alert(getRandomNumber());
+alert(lengthStringCheck());
+
+
 

bad2d1054adcbea9c05e8c20356e99b95ef1b755
gladosq <gladosq@gmail.com>
2021-03-06 18:12:38 +0400

    ...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import {generatePhotos} from './data.js';
 import {createPreviewElements} from './previews.js';
 import {onClickPreview} from './big-picture.js';
-import './upload.js';
+import {changePreviewEffect} from './upload.js';
 
 const photos = generatePhotos();
 
 createPreviewElements(photos);
 
 onClickPreview(photos);
+
+changePreviewEffect();
--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,5 @@
+import 'https://unpkg.com/nouislider@14.6.3/distribute/nouislider.min.js';
+
 const imgInput = document.querySelector('#upload-file');
 const imgEditor = document.querySelector('.img-upload__overlay');
 const cancelButton = document.querySelector('#upload-cancel');
@@ -22,50 +24,34 @@ function hideEditor () {
   imgInput.value = '';
 }
 
-cancelButton.addEventListener('click', function() {
-  hideEditor();
-});
-
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    hideEditor();
+function hideEditorHandler (e) {
+  if (e.type === 'keydown' && e.key !== 'Escape') {
+    return;
   }
-});
+  hideEditor();
+}
+
+cancelButton.addEventListener('click', hideEditorHandler);
+
+
+function changeValue (step) {
+  imgUpldoadPreview.classList.remove('scale-' + scaleControlValue + '-percent');
+
+  let newValue = parseInt(scaleControl.value) + step;
+  scaleControl.value = newValue + '%';
+  scaleControlValue = newValue;
+  imgUpldoadPreview.classList.add('scale-' + newValue + '-percent');
+}
 
 smallerButton.addEventListener('click', function() {
-  if (scaleControl.value == '100%') {
-    scaleControl.value = '75%';
-    scaleControlValue == 75;
-    imgUpldoadPreview.classList.add('scale-75-percent');
-  } else if (scaleControl.value == '75%') {
-    scaleControl.value = '50%';
-    scaleControlValue == 50;
-    imgUpldoadPreview.classList.remove('scale-75-percent');
-    imgUpldoadPreview.classList.add('scale-50-percent');
-  } else if (scaleControl.value == '50%') {
-    scaleControl.value = '25%';
-    scaleControlValue == 25;
-    imgUpldoadPreview.classList.remove('scale-50-percent');
-    imgUpldoadPreview.classList.add('scale-25-percent');
+  if (scaleControl.value != '25%') {
+    changeValue(-25);
   }
 });
 
 biggerButton.addEventListener('click', function() {
-  if (scaleControl.value == '25%') {
-    scaleControl.value = '50%';
-    scaleControlValue == 50;
-    imgUpldoadPreview.classList.remove('scale-25-percent');
-    imgUpldoadPreview.classList.add('scale-50-percent');
-  } else if (scaleControl.value == '50%') {
-    scaleControl.value = '75%';
-    scaleControlValue == 75;
-    imgUpldoadPreview.classList.remove('scale-50-percent');
-    imgUpldoadPreview.classList.add('scale-75-percent');
-  } else if (scaleControl.value == '75%') {
-    scaleControl.value = '100%';
-    scaleControlValue == 100;
-    imgUpldoadPreview.classList.remove('scale-75-percent');
-    imgUpldoadPreview.classList.add('scale-100-percent');
+  if (scaleControl.value != '100%') {
+    changeValue(25);
   }
 });
 
@@ -79,70 +65,73 @@ noUiSlider.create(effectsSlider, {
   connect: 'lower',
 });
 
-
+const previewEffects = {
+  'effect-chrome': {
+    class: 'effects__preview--chrome',
+    filter: 'grayscale(',
+    minValue: 0,
+    maxValue: 1,
+    start: 1,
+    step: 0.1,
+  },
+  'effect-sepia': {
+    class: 'effects__preview--sepia',
+    filter: 'sepia(',
+    minValue: 0,
+    maxValue: 1,
+    start: 1,
+    step: 0.1,
+  },
+  'effect-marvin': {
+    class: 'effects__preview--marvin',
+    filter: 'marvin(',
+    minValue: 0,
+    maxValue: 100,
+    start: 100,
+    step: 1,
+  },
+  'effect-phobos': {
+    class: 'effects__preview--phobos',
+    filter: 'phobos(',
+    minValue: 0,
+    maxValue: 3,
+    start: 3,
+    step: 0.1,
+  },
+  'effect-heat': {
+    class: 'effects__preview--phobos',
+    filter: 'phobos(',
+    minValue: 1,
+    maxValue: 3,
+    start: 3,
+    step: 0.1,
+  },
+}
 
 function changePreviewEffect () {
   let effects = document.querySelectorAll('.effects__radio');
   effectsSlider.classList.add('hidden');
   effects.forEach((effect) => {
     effect.addEventListener('change', function() {
-      if (effect.id == 'effect-chrome') {
-        effectsSlider.classList.remove('hidden');
-        imgUpldoadPreview.className = 'img-upload__preview';
-        imgUpldoadPreview.classList.add('effects__preview--chrome');
-        changeSliderOptions(0, 1, 1, 0.1);
-        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-          effectLevelValue.value = unencoded[handle];
-          imgUpldoadPreview.style.filter = `grayscale(${effectLevelValue.value})`;
-        })
-      } else if (effect.id == 'effect-sepia') {
-        effectsSlider.classList.remove('hidden');
-        imgUpldoadPreview.className = 'img-upload__preview';
-        imgUpldoadPreview.classList.add('effects__preview--sepia');
-        changeSliderOptions(0, 1, 1, 0.1);
-        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-          effectLevelValue.value = unencoded[handle];
-          imgUpldoadPreview.style.filter = `sepia(${effectLevelValue.value})`;
-        })
-      } else if (effect.id == 'effect-marvin') {
-        effectsSlider.classList.remove('hidden');
-        imgUpldoadPreview.className = 'img-upload__preview';
-        imgUpldoadPreview.classList.add('effects__preview--marvin');
-        changeSliderOptions(0, 100, 100, 1);
-        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-          effectLevelValue.value = unencoded[handle];
-          imgUpldoadPreview.style.filter = `invert(${effectLevelValue.value}%)`;
-        })
-      } else if (effect.id == 'effect-phobos') {
-        if (effectsSlider.classList.contains('hidden')) {
-          effectsSlider.classList.remove('hidden');
-        }
-        imgUpldoadPreview.className = 'img-upload__preview';
-        imgUpldoadPreview.classList.add('effects__preview--phobos');
-        changeSliderOptions(0, 3, 3, 0.1);
-        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-          effectLevelValue.value = unencoded[handle];
-          imgUpldoadPreview.style.filter = `blur(${effectLevelValue.value}px)`;
-        })
-      } else if (effect.id == 'effect-heat') {
-        effectsSlider.classList.remove('hidden');
-        imgUpldoadPreview.className = 'img-upload__preview';
-        imgUpldoadPreview.classList.add('effects__preview--heat');
-        changeSliderOptions(1, 3, 3, 0.1);
-        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-          effectLevelValue.value = unencoded[handle];
-          imgUpldoadPreview.style.filter = `brightness(${effectLevelValue.value})`;
-        })
-      } else {
+      if (effect.id == 'effect-none') {
         effectsSlider.classList.add('hidden');
         imgUpldoadPreview.style.filter = 'none';
+      } else {
+        effectsSlider.noUiSlider.off();
 
+        effectsSlider.classList.remove('hidden');
+        imgUpldoadPreview.className = 'img-upload__preview';
+        imgUpldoadPreview.classList.add(previewEffects[effect.id].class);
+        changeSliderOptions(previewEffects[effect.id].minValue, previewEffects[effect.id].maxValue, previewEffects[effect.id].start, previewEffects[effect.id].step);
+
+        effectsSlider.noUiSlider.on('update', (_, handle, unencoded) => {
+          effectLevelValue.value = unencoded[handle];
+          imgUpldoadPreview.style.filter = previewEffects[effect.id].filter + effectLevelValue.value + ')';
+        })
       }
     })
   })
 }
-
-changePreviewEffect();
 
 function changeSliderOptions (minValue, maxValue, start, step) {
   effectsSlider.noUiSlider.updateOptions({
@@ -154,3 +143,5 @@ function changeSliderOptions (minValue, maxValue, start, step) {
     step: step,
   })
 }
+
+export {changePreviewEffect};

0726920ccd0f7ea6fe9e7b637279b0569cc00c20
gladosq <gladosq@gmail.com>
2021-03-14 16:29:04 +0400

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -2,11 +2,28 @@ import {generatePhotos} from './data.js';
 import {createPreviewElements} from './previews.js';
 import {onClickPreview} from './big-picture.js';
 import {changePreviewEffect} from './upload.js';
+import {checkSubmitForm} from './upload-form.js';
+import {createFetch} from './render-server.js';
+import {getData, sendData} from './api.js';
 
 const photos = generatePhotos();
 
-createPreviewElements(photos);
-
-onClickPreview(photos);
-
 changePreviewEffect();
+
+// getData((photos) => {
+
+// });
+
+const fetchPhotos = createFetch(
+  (photos) => {
+    console.log(photos);
+    createPreviewElements(photos);
+    onClickPreview(photos);
+  },
+  (err) => {
+    console.log(err);
+    alert('Произошла ошибка загрузки данных');
+});
+
+checkSubmitForm();
+fetchPhotos();

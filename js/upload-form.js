b991c3c92049d6dfe636b12cfa68f1a24975df1c
gladosq <gladosq@gmail.com>
2021-03-21 17:36:45 +0400

    Перламутровые пуговицы (часть 2)

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,9 +1,9 @@
 import {sendData} from './api.js';
+import {closeSuccessForm, showErrorModal} from './upload.js';
 
 const hashtagsInput = document.querySelector('.text__hashtags');
 const submitForm = document.querySelector('.img-upload__form');
 
-// const MIN_HASHTAG_LENGTH = 3;
 const MAX_HASHTAG_LENGTH = 20;
 const MAX_HASHTAGS_COUNT = 5;
 
@@ -20,37 +20,77 @@ function checkValidityHashtag (hashtag) {
   return true;
 }
 
-hashtagsInput.addEventListener('input', () => {
+let possibilitySubmit = true;
+hashtagsInput.addEventListener('input', (evt) => {
   let hashtags = hashtagsInput.value.split(' ');
 
   hashtags.forEach((hashtag) => {
-    if (hashtag[0] != '#') {
+    let lowerCaseValue = hashtag.toLowerCase();
+
+    if (lowerCaseValue[0] != '#') {
       hashtagsInput.setCustomValidity('Хэштег должен начинаться с решётки');
-    } else if (checkValidityHashtag(hashtag) == false) {
+      possibilitySubmit = false;
+    } else if (checkValidityHashtag(lowerCaseValue) == false) {
       hashtagsInput.setCustomValidity('Введены некорректные символы');
-    } else if (hashtag.length > MAX_HASHTAG_LENGTH) {
+      possibilitySubmit = false;
+    } else if (lowerCaseValue.length > MAX_HASHTAG_LENGTH) {
       hashtagsInput.setCustomValidity('Превышена максимальная длина хэштега');
+      possibilitySubmit = false;
     } else if (hashtags.length > MAX_HASHTAGS_COUNT) {
       hashtagsInput.setCustomValidity('Максимальное кол-во хэштегов - 5');
+      possibilitySubmit = false;
+    } else if (lowerCaseValue.length > 1 && invalidSymbols.includes(evt.data)) {
+      hashtagsInput.setCustomValidity('Введены некорректные символы');
+      possibilitySubmit = false;
     } else {
       hashtagsInput.setCustomValidity('');
+      possibilitySubmit = true;
+    }
+
+    if (hashtagsInput.value == '') {
+      hashtagsInput.setCustomValidity('');
+      possibilitySubmit = true;
     }
   })
 
   hashtagsInput.reportValidity();
 });
 
+function checkSubmitForm (hashtags) {
+  let hashtagsValues = hashtags.split(' ');
+
+  hashtagsValues.forEach((hashtag) => {
+    if (hashtagsValues.indexOf(hashtag) !== hashtagsValues.lastIndexOf(hashtag)) {
+      hashtagsInput.setCustomValidity('Хэштеги не должны повторяться');
+      possibilitySubmit = false;
+    } else if (hashtag == '#') {
+      hashtagsInput.setCustomValidity('Хэштеги не должен состоять из одной решётки');
+      possibilitySubmit = false;
+    }
+
+    if (possibilitySubmit) {
+      hashtagsInput.style.borderColor = '';
+      hashtagsInput.style.color = '';
+      hashtagsInput.style.outline = '';
+    } else {
+      hashtagsInput.style.borderColor = 'tomato';
+      hashtagsInput.style.color = 'tomato';
+      hashtagsInput.style.outline = '2px solid tomato';
+    }
+  })
+}
 
 submitForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
+  checkSubmitForm(hashtagsInput.value);
 
   const formData = new FormData(evt.target);
 
   sendData(
-    () => onSuccess(),
-    () => () => {
-      alert('Данные заполнены не верно')
-    },
+    () => closeSuccessForm(),
+    () => showErrorModal(),
     new FormData(evt.target),
   );
+
+  hashtagsInput.reportValidity();
 });
